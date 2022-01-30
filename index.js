c11eca555f70b5cabbd3e6cd184f63e02746bd38
Khaleed Uddin <uddin1786@gmail.com>
2022-01-30 14:27:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,35 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+  let number = '';
+let operation = process.argv[2];
+if(operation !== 'plus' || operation !== 'minus'){
+    console.log('No operation provided...');
+}
+
+for(i = 3; i < process.argv.length; i++){
+    if(operation === 'plus'){
+    number += process.argv[i];
+    }else if( operation === 'minus'){
+        number -= process.argv[i];
+    }
+  
+}
+  return number  
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;
